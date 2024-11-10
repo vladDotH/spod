eb7a06e0b20cb6ee8fe0a72f4223469894f68701
Vlad <vladbolkunovv@gmail.com>
2024-11-10 21:14:09 +0300

    fix(lab5): times

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,30 +1,42 @@
 #include <mpi.h>
 
-#include <array>
 #include <ctime>
 #include <iostream>
-#include <vector>
 
 int main(int argc, char **argv) {
   MPI::Init(argc, argv);
   MPI::Intracomm &worldComm = MPI::COMM_WORLD;
 
+  double time = 0, start, end;
+
   const int procRank = worldComm.Get_rank();
   const int procCount = worldComm.Get_size();
 
   const int N = (procRank + 1) % 2;
-  const double A = 3.0 + 0.1 * procRank;
+  const int color = N == 1 ? 1 : MPI::UNDEFINED;
 
-  MPI::Intracomm groupComm = worldComm.Split(N, procRank);
+  start = MPI::Wtime();
+  MPI::Intracomm groupComm = worldComm.Split(color, MPI::UNDEFINED);
+  end = MPI::Wtime();
+  time += end - start;
 
   if (N == 1) {
     const int groupRank = groupComm.Get_rank();
+    const double A = 3.0 + 0.1 * groupRank;
     std::cout << "[" << procRank << "/" << groupRank << "] A = " << A
               << std::endl;
     double buf;
+
+    start = MPI::Wtime();
     groupComm.Allreduce(&A, &buf, 1, MPI::DOUBLE, MPI::SUM);
+    end = MPI::Wtime();
+    time += end - start;
+
     std::cout << "[" << procRank << "/" << groupRank << "] Result = " << buf
               << std::endl;
+
+    std::cout << "[" << procRank << "/" << groupRank << "] Time: " << time
+              << "s" << std::endl;
   }
 
   MPI::Finalize();
