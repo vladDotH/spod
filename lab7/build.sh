5dcde852c5ff620f8a775ab3150270c87ff2edab
Vlad <vladbolkunovv@gmail.com>
2024-11-30 23:49:23 +0300

    fix(lab7/build): O2 flags

--- lab7/build.sh
+++ lab7/build.sh
@@ -1,2 +1,2 @@
-g++ main.sequential.cpp -o lab7-sequential
-mpicxx.openmpi main.parallel.cpp -o lab7-parallel
+g++ main.sequential.cpp -o lab7-sequential -O2
+mpicxx.openmpi main.parallel.cpp -o lab7-parallel -O2
